c4fda521a62aafb0174ee60f4b7c7ebf6965ba34
unknown <anilkumarchokkakula38@gmail.com>
2026-02-22 18:35:20 +0530

    fix formatting

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,13 +1,13 @@
 module "cloud_run_service" {
   source = "../modules/cloud_run"
 
-  project_id   = var.project_id
-  service_name = var.service_name
-  region       = var.region
-  image        = var.image
-  environment  = "dev"
-  cpu          = var.cpu
-  memory       = var.memory
+  project_id    = var.project_id
+  service_name  = var.service_name
+  region        = var.region
+  image         = var.image
+  environment   = "dev"
+  cpu           = var.cpu
+  memory        = var.memory
   min_instances = var.min_instances
   max_instances = var.max_instances
 
